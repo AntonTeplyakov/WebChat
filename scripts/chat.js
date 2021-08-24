e5841d6e4d49166a2b373abd9974622be7512c5d
AntonTeplyakov <jsxscript@gmail.com>
2021-08-24 17:44:10 +1000

    get rooms

--- scripts/chat.js
+++ scripts/chat.js
@@ -3,6 +3,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
     async AddChat(message) {
         const now = new Date();
@@ -16,8 +17,9 @@ class Chatroom {
         return response;
     }
     getChats(callback) {
-        this.chats
+        this.unsub = this.chats
         .where('room', '==', this.room)
+        .orderBy('created_at')
         .onSnapshot(snapshot => {
             snapshot.docChanges().forEach(change=>{
                 if(change.type === 'added'){
@@ -26,6 +28,16 @@ class Chatroom {
             });
         });
     }
+    uodateName(username){
+        this.username = username;
+    };
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated');
+        if (this.unsub){
+            this.unsub();
+        }
+    };
 }
 
 const chatroom = new Chatroom('general', 'shaun');
@@ -33,6 +45,10 @@ const chatroom = new Chatroom('general', 'shaun');
 chatroom.getChats((data)=>{
     console.log(data);
 })
+
+setTimeout(()=>{
+    
+},3000);
 
 // chatroom.AddChat('Hello everyone')
 //     .then(() => console.log('chat added'))
