46fd427c9068737c021a3c6c8355d1ea9bd95bf9
AntonTeplyakov <jsxscript@gmail.com>
2021-08-26 15:02:48 +1000

    add new messages

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,14 @@
 const chatlist = document.querySelector('.chat-list')
+const newChatForm = document.querySelector('.new-chat');
+
+newChatForm.addEventListener('submit',e =>{
+    e.preventDefault();
+
+    const message = newChatForm.message.value.trim();
+    chatroom.AddChat(message)
+    .then(()=>newChatForm.reset())
+    .catch(err => console.log(err));
+});
 
 const chatUI = new ChatUI(chatlist);
 const chatroom = new Chatroom('general', 'shaun');
