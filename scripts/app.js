f8af5d747c22759f9720e8d3638a158e8319dd95
AntonTeplyakov <jsxscript@gmail.com>
2021-08-27 15:34:09 +1000

    add change rooms!

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,10 @@
 const chatlist = document.querySelector('.chat-list')
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+
+
 
 newChatForm.addEventListener('submit',e =>{
     e.preventDefault();
@@ -17,10 +21,23 @@ newNameForm.addEventListener('submit',e=>{
     const newName = newNameForm.name.value.trim();
     chatroom.updateName(newName);
     newNameForm.reset();
+
+    updateMssg.innnerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMssg.innnerText = '', 3000);
 });
 
+rooms.addEventListener('click',e=>{
+    if(e.target.tagName === 'BUTTON'){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+});
+
+const username = localStorage.username ? localStorage.username : 'anon'
+
 const chatUI = new ChatUI(chatlist);
-const chatroom = new Chatroom('general', 'shaun');
+const chatroom = new Chatroom('general', username);
 
 
 chatroom.getChats((data)=>{
